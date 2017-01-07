7eb22088fbb484bb23b1cba0fe6d35c92b00a8b8
CHJ <alskdiel@gmail.com>
2017-01-08 00:44:34 +0900

    added bootstrap added modals
    working on modals with sidebar

--- Gemfile
+++ Gemfile
@@ -4,14 +4,16 @@ source 'https://rubygems.org'
 gem 'devise'
 gem 'paperclip'
 gem 'pry'
-gem 'bootstrap-sass'
+gem 'bootstrap-sass', '~> 3.3.6'
+gem 'sass-rails', '>= 3.2'
+gem 'sprockets-rails', :require => 'sprockets/railtie'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.7'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 5.0'
+# gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- app/assets/javascripts/layouts.js
+++ app/assets/javascripts/layouts.js
@@ -1,24 +1,33 @@
 $(document).ready(function(){
 
-  $modal_background = $('.modal-background');
+  $side_bar_bg = $('.side-bar-background');
   $side_bar = $('.side-bar');
 
   $('.nav-bar .menu').on('click', function(){
     openSidebar();
   });
 
-  $('.side-bar .close').on('click', function(){
+  $('.side-bar .cancel').on('click', function(){
     closeSidebar();
+  });
+
+  $('.side-bar .user-info').on('click', function(){
+    $("#modal-sign-in").modal();
+  });
+
+  $('.side-bar .followings').on('click', function(){
+    $("#modal-test").modal();
   });
 
 
   function openSidebar() {
-    $modal_background.addClass('sidebar-opened');
+    $side_bar_bg.addClass('sidebar-opened');
     $side_bar.addClass('opened');
   }
 
   function closeSidebar() {
-    $modal_background.removeClass('sidebar-opened');
+    $side_bar_bg.removeClass('sidebar-opened');
     $side_bar.removeClass('opened');
   }
+
 });
