c179145a5a9362579ba3746f6d2ac9d4f885a06b
CHJ <alskdiel@gmail.com>
2017-01-13 13:22:20 +0900

    customized confirmations controller

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -9,6 +9,17 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   #   super
   # end
 
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      set_flash_message!(:notice, :confirmed)
+      respond_with_navigational(resource){ redirect_to root_path }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ redirect_to root_path }
+    end
+  end
   # GET /resource/confirmation?confirmation_token=abcdef
   # def show
   #   super
