d0e270e53fe4fa9e1562b82d3e247dd2c74ab650
CHJ <alskdiel@gmail.com>
2017-02-16 22:08:35 +0900

    modified nav css design confirm

--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -1,5 +1,5 @@
 class UserInfo < ActiveRecord::Base
-  has_attached_file :cover_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :cover_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/cover_missing.png"
   validates_attachment_content_type :cover_image, content_type: /\Aimage\/.*\z/
 
   belongs_to :user
